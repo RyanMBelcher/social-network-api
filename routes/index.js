2060c7f5bea911748607b8d6c39dc770b0c15b37
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-13 17:38:48 -0500

    setup routes index

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
-// /api routes
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    return res.send('Wrong route!');
+});
+
+module.exports = router;
