89686b8f69c21a3551475013fa9be8e2d007713a
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-13 17:41:33 -0500

    setup api routes index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,12 @@
 // /api/users
 
 //  /api/thoughts
+
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
