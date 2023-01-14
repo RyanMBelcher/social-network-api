2c85479d188be93510911c88fae62cd7cd794eb0
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-14 14:04:19 -0500

    finished reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -17,3 +17,30 @@
 //     Use a getter method to format the timestamp on query
 
 // This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 80,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+
+module.exports = reactionSchema;
