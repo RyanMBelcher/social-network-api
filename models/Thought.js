a1b22336724a4597dcaf3c06551cec391f7db345
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-14 13:54:25 -0500

    finished thought model

--- models/Thought.js
+++ models/Thought.js
@@ -15,4 +15,42 @@
 // reactions (These are like replies)
 //     Array of nested documents created with the reactionSchema
 
+
+const { Schema, model } = require('mongoose');
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [Reaction],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought
