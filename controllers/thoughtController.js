a0b65f7538a1748745f31a8035f651d683d628aa
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-01-15 18:50:43 -0500

    get thought, get thought by id, create thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -14,3 +14,37 @@
 // .findOneAndUpdate() ??? { $addToSet }
 // removeReaction,
 // .findOneAndUpdate() ??? { $pull }
+
+const { Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thought = await Thought.find();
+            console.log(thought);
+            return res.json(thought);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+            const singleThought = await Thought.findOne();
+            return !singleThought
+                ? res.status(404).json({ message: 'No thought with this id!' })
+                : res.json(singleThought);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+            const thoughtCreate = await Thought.create(req.body);
+            return res.json(thoughtCreate);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+};
